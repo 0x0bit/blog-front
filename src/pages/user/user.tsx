c46478e19f16bbe9a825478ec8cbb1ff92ece880
bo.yang <bo.yang@upai.com>
2020-03-31 16:39:17 +0800

    fix: 关于博主页面搭建

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
-
+import {Row, Col, Card} from 'antd';
 
 
 export default class User extends React.Component<any, any>{
   render() {
     return (
       <div className="about-user">
-        <h2>关于博主</h2>
+        <Row justify="center">
+          <Col span={18} className="about-content">
+            <Card title="关于博主" bordered={true}>
+              <div className="about-me">
+                <div className="about-me-title"><h2>个人介绍</h2></div>
+                <div className="about-me-content">
+
+                </div>
+              </div>
+            </Card>
+          </Col>
+        </Row>
       </div>
     )
   }
